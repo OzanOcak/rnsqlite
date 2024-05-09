bed55b8d9d5a0688592ef31b892b4033ad8dfbad
OzanOcak <ozanocak@gmail.com>
2024-05-09 12:41:24 -0400

    fetching db

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,7 +1,26 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Category, Transaction } from "../types";
+import { useSQLiteContext } from "expo-sqlite";
 
 const Home = () => {
+  const [categories, setCategories] = React.useState<Category[]>([]);
+  const [transactions, setTransactions] = React.useState<Transaction[]>([]);
+
+  const db = useSQLiteContext();
+
+  React.useEffect(() => {
+    db.withTransactionAsync(async () => {
+      await getData();
+    });
+  }, [db]);
+
+  async function getData() {
+    const result = await db.getAllAsync<Transaction>(
+      `SELECT * FROM Transactions ORDER BY date DESC;`
+    );
+    console.log(result);
+  }
   return (
     <View>
       <Text>Home</Text>
